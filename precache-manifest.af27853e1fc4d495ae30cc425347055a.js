aac95dc15d183ea14f43b2bd65d49746dd6de1b5
Muhammad chandra zulfikar <muhammadchandrazulfikar@Muhammads-MacBook-Pro.local>
2019-04-10 21:52:53 +0700

    Updates

---
+++ precache-manifest.af27853e1fc4d495ae30cc425347055a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/news-react-v2/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c1106ef0d59cb733fbe4",
+    "url": "/news-react-v2/static/js/runtime~main.8bb81e84.js"
+  },
+  {
+    "revision": "7d59d370c7af392b1e98",
+    "url": "/news-react-v2/static/js/main.aef73203.chunk.js"
+  },
+  {
+    "revision": "c7039e4e58f2bdd72e50",
+    "url": "/news-react-v2/static/js/2.6e6c3edd.chunk.js"
+  },
+  {
+    "revision": "7d59d370c7af392b1e98",
+    "url": "/news-react-v2/static/css/main.4b935dde.chunk.css"
+  },
+  {
+    "revision": "fc1f360c442d9f2a73e8e55eb892a602",
+    "url": "/news-react-v2/index.html"
+  }
+];
